504deb9cbfec7c5b4c8a615cff07e5220333055c
Zhu Zhanyan <program.nom@gmail.com>
2024-09-06 11:08:23 +0800

    feat(class_1): add qsub script used to submit job to aspire 2A

---
+++ class_1/qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N sc2001_class_1
+#PBS -l select=1:ncpus=128:mem=150gb
+#PBS -l walltime=00:20:00
+#PBS -o sc2001_class_1.log
+#PBS -j oe
+#PBS -q normal
+
+set -ex -o pipefail
+cd $PBS_O_WORKDIR
+
+# install pypy
+curl https://downloads.python.org/pypy/pypy3.10-v7.3.17-linux64.tar.bz2 | tar xjf -
+export PATH="$PBS_O_WORKDIR/pypy3.10-v7.3.17-linux64/bin:$PATH"
+
+time pypy3 code.py

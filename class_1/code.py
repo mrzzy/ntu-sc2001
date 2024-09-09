5439186db07d99fbe6c415a076bbc65b6f86c555
Zhu Zhanyan <program.nom@gmail.com>
2024-09-09 12:26:20 +0800

    fix(class_1): param s not passed to hybrid_sort()
    This leads to mergesort time complexity in all trials

--- class_1/code.py
+++ class_1/code.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
             # try out values for param s
             for s in range(1, 128 + 1):
                 # run multiple trials per (n, s) combination
-                for t in range(5):
+                for t in range(30):
                     # skip trial if already done
                     if (n, s, t) in done:
                         n_skip += 1
